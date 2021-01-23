d2bdd5973ed235f9cc7c998e58860263d5912f70
sueda akın <sueda_akin@hotmail.com>
2021-01-24 02:06:26 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+" adlı musteri eklendi");
+            Console.WriteLine("---------------------");
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Musterinin adı: "+musteri.Ad);
+            Console.WriteLine("Musterinin adı: "+musteri.Soyad);
+            Console.WriteLine("Musterinin adı: "+musteri.Telefon);
+            Console.WriteLine("---------------------");
+        }
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " adlı musteri silindi");
+            Console.WriteLine("---------------------");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Sueda";
+            musteri1.Soyad = "Akın";
+            musteri1.Telefon = 4758962881;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Elif";
+            musteri2.Soyad = "Akın";
+            musteri2.Telefon = 1258962881;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Listele(musteri1);
+            musteriManager.Listele(musteri2);
+            musteriManager.Silme(musteri1);
+            musteriManager.Silme(musteri2);
+
+            Console.ReadLine();
+        }
+    }
+}
